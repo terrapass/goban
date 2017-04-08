6468c5ccd186a3fc3ef6a895ba4c2e2b2b8e6dd5
Taras Denysenko <terrapass93@gmail.com>
2017-04-08 19:00:38 +0300

    Added estimateConfidence() method to recognizer.

--- goban/recognition.py
+++ goban/recognition.py
@@ -9,7 +9,8 @@ import numpy as np;
 
 from goban.board import CellType
 from goban.vision.segmentation import Segmentation
-from goban.nnutils import interpret, asInputVector
+from goban.nnutils import interpret, asInputVector, ConfidenceMeasure
+from random import uniform as rand
 
 class AbstractBoardRecognizer:
     def recognize(boardImage):
@@ -28,9 +29,40 @@ class NeuralTopdownFixedSizeBoardRecognizer(AbstractBoardRecognizer):
         for i in range(0, self.boardHeight):
             for j in range(0, self.boardWidth):
                 # Classify using network
-                classification = interpret(self.network.feedforward(asInputVector(segmentation[(i,j)])))
-                rawBoard[i,j], confidence = classification[0].value, classification[1];
+                rawBoard[i,j], confidence = self.__classify(segmentation[(i,j)]);
                 sumConfidence += confidence;
         board = np.vectorize(CellType)(rawBoard);
         avgConfidence = sumConfidence / (self.boardWidth * self.boardHeight);
         return board, avgConfidence;
+
+    def estimateConfidence(self, topdownBoardImage, samples = None):
+        segmentation = Segmentation(topdownBoardImage, self.boardHeight, self.boardWidth);
+        sumConfidence = 0.0;
+
+        if samples == None:
+            # Classify all segments and return average confidence
+            for i in range(0, self.boardHeight):
+                for j in range(0, self.boardWidth):
+                    # Classify using network
+                    sumConfidence += self.__classify(segmentation[i,j])[1];
+            avgConfidence = sumConfidence / (self.boardWidth * self.boardHeight);
+        else:
+            # Take the requested number of random fragments and
+            # return average confidence of just their classification
+            #minConfidence = 1.0;
+            for k in range(0, samples):
+                i = int(rand(0, self.boardWidth - 1))
+                j = int(rand(0, self.boardHeight - 1))
+                # Classify using network
+                sumConfidence += self.__classify(segmentation[(i,j)])[1];
+#                if confidence < minConfidence:
+#                    minConfidence = confidence;
+            avgConfidence = sumConfidence / samples;
+            #return board, avgConfidence - minConfidence
+        return avgConfidence;
+    
+    def __classify(self, fragment, confidenceMeasure = ConfidenceMeasure.MAX_OUTPUT):
+        # Classify using network
+        classification = interpret(self.network.feedforward(asInputVector(fragment)), 0.0, confidenceMeasure)
+        cellTypeValue, confidence = classification[0].value, classification[1];
+        return cellTypeValue, confidence
